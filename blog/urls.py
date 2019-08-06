28a01a39018317f9eac923af13fd4fe9178a1388
JackXuZhaoLong <xuzhaolongwang@163.com>
2019-08-07 00:40:31 +0800

    myblog

---
+++ blog/urls.py
@@ -0,0 +1,8 @@
+from django.urls import  path
+from . import views
+
+#http://localhost:8000/blog
+urlpatterns =[
+    path('<blog_pk>',views.blog_detail, name='blog_detail'),
+    path('type/<int:blog_type_pk>',views.blog_with_type, name='blog_with_type'),
+]
