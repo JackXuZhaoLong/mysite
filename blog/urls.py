57e5c24e95ff1c590ab99ebd1f6769c92257af05
JackXuZhaoLong <xuzhaolongwang@163.com>
2019-08-09 00:19:05 +0800

    新增模板嵌套

--- blog/urls.py
+++ blog/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 #http://localhost:8000/blog
 urlpatterns =[
+    path('',views.blog_list, name='blog_list'),
     path('<blog_pk>',views.blog_detail, name='blog_detail'),
     path('type/<int:blog_type_pk>',views.blog_with_type, name='blog_with_type'),
 ]
